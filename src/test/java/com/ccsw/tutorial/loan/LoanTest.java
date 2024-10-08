e763b7d543da569024c64b0edfc52ed7a07f657e
spenavil <sandra.pena@estudiantat.upc.edu>
2024-10-08 12:55:56 +0200

    test

--- src/test/java/com/ccsw/tutorial/loan/LoanTest.java
+++ src/test/java/com/ccsw/tutorial/loan/LoanTest.java
@@ -1,12 +1,20 @@
 package com.ccsw.tutorial.loan;
 
+import com.ccsw.tutorial.clients.ClientsService;
+import com.ccsw.tutorial.clients.model.ClientsDto;
+import com.ccsw.tutorial.common.exceptions.Exceptions;
+import com.ccsw.tutorial.game.GameService;
+import com.ccsw.tutorial.game.model.GameDto;
 import com.ccsw.tutorial.loan.model.Loan;
+import com.ccsw.tutorial.loan.model.LoanDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +28,90 @@ public class LoanTest {
 
     @InjectMocks
     private LoanServiceImpl loanService;
+
+    @Mock
+    private GameService gameService;
+
+    @Mock
+    private ClientsService clientsService;
+
+    @Test
+    public void testSaveThrowsExceptionWhenEndDateBeforeStartDate() {
+        LoanDto dto = new LoanDto();
+        dto.setDateStart(new Date(System.currentTimeMillis() + 86400000)); // mañana
+        dto.setDateEnd(new Date(System.currentTimeMillis())); // hoy
+
+        Exception exception = assertThrows(Exceptions.class, () -> {
+            loanService.save(dto);
+        });
+
+        assertEquals("La fecha de fin no puede ser anterior a la fecha de inicio", exception.getMessage());
+    }
+
+    @Test
+    public void testSaveThrowsExceptionWhenLoanPeriodExceeds14Days() {
+        LoanDto dto = new LoanDto();
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, 16);
+        dto.setDateStart(new Date());
+        dto.setDateEnd(calendar.getTime());
+
+        GameDto gameDto = new GameDto();
+        gameDto.setId(1L);
+        dto.setGame(gameDto);
+
+        ClientsDto clientsDto = new ClientsDto();
+        clientsDto.setId(1L);
+        dto.setClient(clientsDto);
+
+        Exception exception = assertThrows(Exceptions.class, () -> {
+            loanService.save(dto);
+        });
+
+        assertEquals("El periodo de préstamo no puede ser mayor a 14 días", exception.getMessage());
+    }
+
+    /*
+    @Test
+    public void testSaveThrowsExceptionWhenGameAlreadyLoanedInSelectedPeriod() {
+        LoanDto dto = new LoanDto();
+        Calendar calendarStart = Calendar.getInstance();
+        calendarStart.add(Calendar.DAY_OF_YEAR, 1);
+        dto.setDateStart(calendarStart.getTime());
+        Date d = dto.getDateStart();
+        Date e = dto.getDateEnd();
+
+        Calendar calendarEnd = Calendar.getInstance();
+        calendarEnd.add(Calendar.DAY_OF_YEAR, 5);
+        dto.setDateEnd(calendarEnd.getTime());
+
+        GameDto gameDto = new GameDto();
+        gameDto.setId(1L);
+        dto.setGame(gameDto);
+
+        ClientsDto clientDto = new ClientsDto();
+        clientDto.setId(1L);
+        dto.setClient(clientDto);
+
+        // Simulamos que hay un préstamo existente en el período
+        LoanDto existingLoanDto = new LoanDto();
+        ClientsDto clientDto2 = new ClientsDto();
+        clientDto.setId(2L);
+        dto.setClient(clientDto2);
+        existingLoanDto.setGame(gameDto);
+        existingLoanDto.setClient(clientDto2);
+        existingLoanDto.setDateStart(d);
+        existingLoanDto.setDateStart(e);
+
+        // Simula el comportamiento de los métodos find() para devolver préstamos existentes
+        when(loanRepository.find(eq(gameDto.getId()), isNull(), any(Date.class))).thenReturn(Collections.singletonList(existingLoan)); // Simula préstamo existente para la fecha de inicio
+
+        Exception exception = assertThrows(Exceptions.class, () -> {
+            loanService.save(dto);
+        });
+
+        assertEquals("El juego ya está prestado a otro cliente en el periodo de tiempo seleccionado.", exception.getMessage());
+    }*/
 
     @Test
     public void getExistsLoanIdShouldReturnLoan() {
